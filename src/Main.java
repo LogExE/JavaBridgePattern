b100a839ff96f7afda1f591c1e21958afb65c196
LogExE <awesome149712@gmail.com>
2023-03-31 00:06:39 +0400

    prepareFile()

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 
 public class Main {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Database db = new MemoryDatabase();
-        new Adder(db).processData();
+    public static void prepareFile() throws IOException {
         ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream("test.txt"));
         os.writeObject(2);
         os.writeObject(1);
         os.writeObject(2);
-        os.close();
+    }
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Database db = new MemoryDatabase();
+        new Adder(db).processData();
+
+        prepareFile();
         db = new FileDatabase("test.txt");
         new Adder(db).processData();
     }
